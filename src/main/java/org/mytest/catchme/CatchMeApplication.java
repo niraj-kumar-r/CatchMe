719d16d9f5d48b31e6a780d642a5bf0063df2afc
Niraj Kumar <nirajk23523@gmail.com>
2025-05-22 02:22:14 +0530

    Initial Commit

---
+++ src/main/java/org/mytest/catchme/CatchMeApplication.java
@@ -0,0 +1,13 @@
+package org.mytest.catchme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mytest/catchme/CatchMeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mytest.catchme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
